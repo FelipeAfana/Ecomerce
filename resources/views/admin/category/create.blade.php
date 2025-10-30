f381bd956e91d52ef1ebbac6e1c66cd5b8f4543a
FelipeAfana <fafanador@unab.edu.co>
2025-10-30 12:37:01 -0500

    avance de clase 30-10-2025

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <h1> add new category</h1>
+
+    <div class="card">
+        <div class='card-dody'>
+
+            <form action="{{ route('admin.category.store') }}" method="post">
+                @csrf
+
+            <input type="text" name="name">
+
+            <input type="submit" name="Save">
+
+            </form>
+            
+        </div>
+    </div>
+        
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class],'index'
+);
 
 Route::prefix("products")->controller(ProductController::class) ->group(function(){
     Route::get("/", "index");
@@ -23,3 +24,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get("/", [HomeController::class], 'welcom');
+
+Route::prefix('/admin')->group(function(){
+    Route::get('/', [AdminController::class, 'index'])->name("admin.index");
+    Route::get('category/create', [CategoryController::class])->name('admin.category.create');
+    Route::post('categort/store',[CategoryController::class])->name('admin.category.store');
+});
